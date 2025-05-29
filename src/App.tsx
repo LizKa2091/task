1e785d82ef418d626e9ebe1510fa3d4bf67bce0c
lizka <lizka4231@gmail.com>
2025-05-29 23:59:52 +0300

    feat: add styles for app, implement todolist in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
+import ToDoList from './components/ToDoList';
+import './styles/global.scss';
+import styles from './App.module.scss';
 
 const App: FC = () => {
-  return (
-    <>
-      hi
-    </>
-  );
+   return (
+      <div className={styles.container}>
+         <ToDoList />
+      </div>
+   );
 }
 
 export default App;
