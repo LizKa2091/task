3180566e5d3b4a4d1a85e107ed5c8c7c3c11aa58
lizka <lizka4231@gmail.com>
2025-05-30 00:45:51 +0300

    feat: implement add to do item component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
 import ToDoList from './components/ToDoList';
+import AddToDoItem from './components/AddToDoItem';
 import './styles/global.scss';
 import styles from './App.module.scss';
 
 const App: FC = () => {
    return (
       <div className={styles.container}>
+         <AddToDoItem />
          <ToDoList />
       </div>
    );
--- src/components/AddToDoItem.tsx
+++ src/components/AddToDoItem.tsx
@@ -1,9 +1,26 @@
-import React, { FC } from 'react'
+import React, { ChangeEvent, FC, FormEvent, useState } from 'react'
+import { useAddToDo } from '../hooks/useAddToDo';
 
 const AddToDoItem: FC = () => {
+   const [inputValue, setInputValue] = useState<string>('');
+   const { mutate: addToDo } = useAddToDo();
+
+   const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      if (!inputValue.trim()) return;
+
+      addToDo(inputValue, {
+         onSuccess: () => setInputValue('')
+      });
+   }
+
    return (
       <div>
-         
+         <form onSubmit={handleFormSubmit}>
+            <input value={inputValue} onChange={(e: ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)} type="text" placeholder='Введите текст задачи' />
+            <button type='submit'>Создать</button>
+         </form>
       </div>
    )
 }
