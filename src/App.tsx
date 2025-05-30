08b7e55a486eac0795eb5b6159806489e9278ff4
lizka <lizka4231@gmail.com>
2025-05-30 18:29:51 +0300

    feat: add snackbar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import ToDoList from './components/ToDoList';
 import AddToDoItem from './components/AddToDoItem';
 import './styles/global.scss';
 import styles from './App.module.scss';
+import { useSnackbar } from './context/SnackbarContext';
+import Snackbar from './components/Snackbar';
 
 const App: FC = () => {
+   const { message, setMessage, messageType } = useSnackbar();
+
    return (
       <div className={styles.container}>
+         {message &&
+            <Snackbar message={message} setMessage={setMessage} messageType={messageType} />
+         }
          <AddToDoItem />
-         <ToDoList />
+         <ToDoList />         
       </div>
    );
 }
---
+++ src/components/Snackbar.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import styles from './Snackbar.module.scss';
+import { messageTypes } from '../types';
+
+interface ISnackbarProps {
+   message: string;
+   setMessage: (message: string) => void;
+   messageType: messageTypes;
+}
+
+const Snackbar: FC<ISnackbarProps> = ({ message, setMessage, messageType }) => {
+   return (
+      <div className={`${styles.snackbarContainer} ${styles[messageType]}`}>
+         <p>{message}</p>
+         <button onClick={() => setMessage('')} className={styles.snackbarButton}>x</button>
+      </div>
+   )
+}
+
+export default Snackbar;
