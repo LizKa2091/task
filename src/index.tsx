feaf8ab79a3dfe36a1a5b4b1f8c5510bf94afe62
lizka <lizka4231@gmail.com>
2025-05-30 18:28:00 +0300

    feat: add snackbar context provider to root

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
 import queryClient from './queryClient';
 import App from './App';
+import { SnackbarContextProvider } from './context/SnackbarContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <SnackbarContextProvider>
+         <App />
+      </SnackbarContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
